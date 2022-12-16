dfbc9ebf6f3539bc0d49accf66c6ecd8d66d8b0d
Fernando Henrique <89784479+FerHe05@users.noreply.github.com>
2022-12-16 16:43:01 -0300

    Add files via upload

---
+++ Movimento_Personagem/header.h
@@ -0,0 +1,45 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Character.h"
+#include "PersonagemTPS.generated.h"
+
+UCLASS()
+class STRANGERPLACE_API APersonagemTPS : public ACharacter
+{
+	GENERATED_BODY()
+
+public:
+	// Sets default values for this character's properties
+	APersonagemTPS();
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+
+	void MoverParaFrente(float Valor);
+	void MoverDireita(float Valor);
+
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Camera")
+		class UCameraComponent* CameraPersonagem;
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "CameraBoom")
+		class USpringArmComponent* SpringArmCamera;
+	UPROPERTY(BlueprintReadOnly, Category = "Pulo")
+		bool bEstaPulando;
+
+	void Agachar();
+	void Levantar();
+	void Pular();
+	void PararPulo();
+
+public:
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+
+	// Called to bind functionality to input
+	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
+
+};
